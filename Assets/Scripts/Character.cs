2651b99e56e716852864ca6c8ac07369c957b955
Alberto <bertum_1993@hotmail.com>
2018-07-07 18:21:03 +0200

    Weapons controller and weapons

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,10 +3,34 @@
 public class Character : MonoBehaviour
 {
     public bool teleported;
+    private float health;
+    public bool isRight;
 
     private void Awake()
     {
         teleported = false;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var weaponController = collision.gameObject.GetComponent<WeaponController>();
+        if (collision.gameObject.tag == "Bullet")
+        {
+            health -= collision.gameObject.GetComponent<BulletController>().damage;
+        }
+        else if (collision.gameObject.tag == "MeleeWeapon" && weaponController.attacking)
+        {
+            health -= weaponController.damage;
+        }
+    }
+
+    private void GetWeapon()
+    {
+        //Call weaponController set owner
+    }
+
+    private void DropWeapon()
+    {
+
+    }
 }
---
+++ Assets/Scripts/Weapon/BulletController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private bool move = false;
+    public float damage = 0;
+    private float speed = 6;
+    private int number;
+    private float dispersionInt = 0.1f;
+    private Vector3 direction;
+
+    private void Awake()
+    {
+        move = false;
+    }
+
+    public void ActivateAndMove(Vector3 weaponPosition, int number, bool right)
+    {
+        direction = right ? Vector3.right : Vector3.left;
+        this.transform.position = weaponPosition;
+        this.gameObject.SetActive(true);
+        move = true;
+        this.number = number;
+        Destroy(this.gameObject, 5);
+    }
+
+    private void Update()
+    {
+        if (move)
+        {
+            switch (number)
+            {
+                case 0:
+                    this.transform.Translate(Vector3.right * Time.deltaTime * speed);
+                    break;
+                case 1:
+                    this.transform.Translate(Vector3.up * dispersionInt * Time.deltaTime * speed);
+                    this.transform.Translate(Vector3.right * Time.deltaTime * speed);
+                    break;
+                case 2:
+                    this.transform.Translate(Vector3.down * dispersionInt * Time.deltaTime * speed);
+                    this.transform.Translate(Vector3.right * Time.deltaTime * speed);
+                    break;
+            }
+
+        }
+    }
+}
