40cb8103f1212fb03a50fae78058784e55edb0ba
Rubén Romero Jiménez <ruben.rj92@gmail.com>
2018-07-07 18:16:39 +0200

    Control de gravedad general y particular

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,6 +7,55 @@ public class Character : MonoBehaviour
     private void Awake()
     {
         teleported = false;
+        constForce = GetComponent<ConstantForce>();
     }
+
+    public ConstantForce constForce;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            changeMyGravity();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            invertGravity();
+        }
+    }
+
+    public void changeMyGravity()
+    {
+        if (constForce.force.y != 0)
+        {
+            constForce.force = new Vector3(0, 0, 0);
+        }
+        else {
+            if (Physics.gravity.y > 0) { 
+                constForce.force = new Vector3(0, -18.0f, 0);
+            }
+            else if (Physics.gravity.y < 0)
+            {
+                constForce.force = new Vector3(0, 18.0f, 0);
+            }
+        }
+        
+    }
+
+    public void invertGravity () {
+        Physics.gravity = new Vector3(0, Physics.gravity.y * -1);
+        if (constForce.force.y != 0) { constForce.force = new Vector3(0, constForce.force.y * -1, 0); }
+    }
+
 
 }
