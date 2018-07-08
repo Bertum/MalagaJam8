214bf2fa8a61532ac73b3ad609e5f2e17e2c6f84
Alberto <bertum_1993@hotmail.com>
2018-07-08 13:12:07 +0200

    Colliders to kill the players when go out of the camera

---
+++ Assets/Scripts/DeathCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DeathCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
